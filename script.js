ff8f05e4463b3c5f0f8a7e0065ed378559225ade
Kelly Cristina Finardi Esteves <kellycge21@outlook.com>
2025-09-24 21:23:02 -0300

    Criação e upload do README.md e dos demais arquivos

---
+++ script.js
@@ -0,0 +1,6 @@
+const conselhos = [
+    "Acredite em você mesmo." , "O primeiro passo é o mais importante.",
+    "A persistência realiza o impossível.","Aprenda algo novo hoje",
+    "Seja gentil com todos que encontrar."
+]
+console.log("Arquivo de Script carregadp. Array de conselhos pronto.");
